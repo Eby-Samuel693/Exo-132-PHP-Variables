ff1dcc909abe4c2bc2cadcafe9a1423c0b1839a4
Eby-Samuel693 <coquelet.samuel@gmail.com>
2021-01-18 13:45:19 +0100

    kuijyhg

---
+++ php.php
@@ -0,0 +1,15 @@
+<?php
+
+    $texte = "Du texte est stocké ";
+    $nombre = 30 ;
+    $valu = "y vaut ";
+    echo $texte;
+    echo $valu.$nombre;
+    $m = 5;
+    $n = 7;
+    $result = $m + $n;
+    echo $m + $n;
+
+
+    ?>
+
